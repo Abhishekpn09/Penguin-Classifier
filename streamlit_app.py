fea67e6712b3dea5cdff16e39b85391adb408502
Abhishek PN <145152823+Abhishekpn09@users.noreply.github.com>
2025-02-07 12:34:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title(' Machine learning App')
 
 st.write('This app builds an machine learning model')
+
+df=pd.read_csv('https://raw.githubusercontent.com/Abhishekpn09/StreamlitProjects/refs/heads/master/.devcontainer/penguins_clean%20(1).csv')
+
