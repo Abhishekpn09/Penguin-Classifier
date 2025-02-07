79a1ab23613c6060065ce0005d3681717f68a475
Abhishek PN <145152823+Abhishekpn09@users.noreply.github.com>
2025-02-07 13:17:33 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,4 +39,10 @@ data = {'island': island,
           'sex': gender}
 input_df = pd.DataFrame(data, index=[0])
 input_penguins = pd.concat([input_df, X], axis=0)
-input_df
+
+with st.expander('Input features'):
+  st.write('**Input penguin**')
+  input_df
+  st.write('**Combined penguins data**')
+  input_penguins
+
