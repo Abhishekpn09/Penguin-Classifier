9b64a9eb1fad701ca800b060abe51c6506a6034e
Abhishek PN <145152823+Abhishekpn09@users.noreply.github.com>
2025-02-07 12:37:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,7 @@ st.title(' Machine learning App')
 
 st.write('This app builds an machine learning model')
 
-df=pd.read_csv('https://raw.githubusercontent.com/Abhishekpn09/StreamlitProjects/refs/heads/master/.devcontainer/penguins_clean%20(1).csv')
-df
+with st.expander('Data'):
+  st.write('**Raw data**')
+  df=pd.read_csv('https://raw.githubusercontent.com/Abhishekpn09/StreamlitProjects/refs/heads/master/.devcontainer/penguins_clean%20(1).csv')
+  df
